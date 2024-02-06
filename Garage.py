21c7aed8d3ee74af810e49e1d2f407b308d16db9
Slaiden <30178070@nescol.ac.uk>
2024-02-06 15:18:26 +0000

    Started classes

---
+++ Garage.py
@@ -0,0 +1,7 @@
+class Garage:
+    def __init__(self,garageID,garageName,address,town,postcode):
+        self.garageID = garageID
+        self.garageName = garageName
+        self.address = address
+        self.town = town
+        self.postcode = postcode
