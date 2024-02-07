26ec2c68b70069c64ee4152b762f724ef75b12ca
Daniel McPherson <daniel.mcpherson@live.co.uk>
2024-02-07 11:59:51 +0000

    Added methods for adding, deleting and searching

--- Customer.py
+++ Customer.py
@@ -1,3 +1,5 @@
+from Database import cursor, db
+
 class Customer:
     def __init__(self,customerID,forname,surname,address,postcode,phoneNo):
         self.customerID = customerID
@@ -6,3 +8,52 @@ class Customer:
         self.address = address
         self.postcode = postcode
         self.phoneNo = phoneNo
+
+    @staticmethod
+    def addCustomer(customer):
+        cursor.execute("INSERT INTO customer(forname, surname, address, postcode, phoneNo) VALUES (%s,%s,%s,%s,%s);", (customer.forname, customer.surname, customer.address, customer.postcode, customer.phoneNo.replace(" ", "")))
+        db.commit()
+
+    @staticmethod
+    def deleteCustomer(customerID):
+        cursor.execute("DELETE FROM customer WHERE customerID=%s", (customerID,))
+        db.commit()
+
+    @staticmethod
+    def searchCustomer():
+        #  forname > Surname > Phone number
+        forname = input("Please enter your forname: ").lower()
+        cursor.execute("SELECT * FROM customer WHERE forname=%s;", (forname,))
+        
+        customers = cursor.fetchall()
+
+        # If multiple customers exist with that firstnames
+        if len(customers) > 1:
+            surname = input("Please enter your surname: ").lower()
+            cursor.execute("SELECT * FROM customer WHERE forname=%s AND surname=%s;", (forname,surname))
+
+
+            customers = cursor.fetchall()
+            if len(customers) > 1:
+                phoneNo = input("Please enter your phone number: ").lower().replace("\r", "")
+                cursor.execute("SELECT * FROM customer WHERE forname=%s AND surname=%s AND phoneNo=%s;", (forname,surname, phoneNo))
+
+                customers = cursor.fetchall()
+                if len(customers) == 0:
+                    print("No customers with that firstname and surname exists")
+                
+                else:
+                    print(customers)
+
+            elif len(customers) == 0:
+                print("No customers with that firstname and surname exists")
+            
+            else:
+                print(customers)
+
+        elif len(customers) == 0:
+            print("No customers with that firstname exists")
+        
+        else:
+            print(customers)
+
