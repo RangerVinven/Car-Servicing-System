8014e5684203819cdb7aaf04c14fe8ce9847e19d
Daniel McPherson <48417169+RangerVinven@users.noreply.github.com>
2024-02-15 09:38:50 +0000

    Delete Job.py

--- Job.py
+++
@@ -1,8 +0,0 @@
-class Job:
-    def __init__(self,jobID,garageID,dateIn,dateOut,regNo,cost):
-        self.jobID = jobID
-        self.garageID = garageID
-        self.dateIn = dateIn
-        self.dateOut = dateOut
-        self.regNo = regNo
-        self.cost = cost
