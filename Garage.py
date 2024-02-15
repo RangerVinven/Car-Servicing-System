d831b2eb71ba3326924f564cc840c91e4e0a9ff7
Daniel McPherson <48417169+RangerVinven@users.noreply.github.com>
2024-02-15 09:38:29 +0000

    Delete Garage.py

--- Garage.py
+++
@@ -1,7 +0,0 @@
-class Garage:
-    def __init__(self,garageID,garageName,address,town,postcode):
-        self.garageID = garageID
-        self.garageName = garageName
-        self.address = address
-        self.town = town
-        self.postcode = postcode
