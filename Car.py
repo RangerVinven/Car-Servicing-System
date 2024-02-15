40e0fce4e030db8c345e32a65b2ff3a72ffc86d6
Daniel McPherson <48417169+RangerVinven@users.noreply.github.com>
2024-02-15 09:38:21 +0000

    Delete Car.py

--- Car.py
+++
@@ -1,7 +0,0 @@
-class Car:
-    def __init__(self, regNo,make,model,year,customerID):
-        self.regNo = regNo
-        self.make = make
-        self.model = model
-        self.year = year
-        self.customerID = customerID
