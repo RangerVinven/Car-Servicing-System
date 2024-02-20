686b5a77e039e0e6c3b7ade1bc3a99dc9480dc7f
Nuclearturtle12 <84713843+Nuclearturtle12@users.noreply.github.com>
2024-02-20 16:05:25 +0000

    modules and port shit
    adding port 3307 to connector file and adding __init__ files to classes and helpers folders

--- helpers/Database_Connector.py
+++ helpers/Database_Connector.py
@@ -1,4 +1,4 @@
 import mysql.connector
 
-db = mysql.connector.connect(user="root", password="root", host="127.0.0.1", database="carServices")
+db = mysql.connector.connect(user="root", password="root", port=3307, host="127.0.0.1", database="carServices")
 cursor = db.cursor()
---
+++ main.py
@@ -0,0 +1,1 @@
+from classes.Customer import Customer
